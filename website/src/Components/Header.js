88cffd42a74cd8d9ad55271f3633072d77cc8357
Sunny You <yousunny3@gmail.com>
2025-02-06 10:54:55 +0900

    i was bored

--- website/src/Components/Header.js
+++ website/src/Components/Header.js
@@ -10,10 +10,10 @@ import Home from "../Pages/Home";
 import About from "../Pages/About";
 import AboutContact from "../Pages/Contact";
 import Story from "../Pages/Story";
-import Story from "../Pages/Projects";
+import Projects from "../Pages/Projects";
 import { useState } from "react";
 
-const navItems = ['Home', 'Story' ,'About', 'Contact'];
+const navItems = ['Home', 'Story' ,'About', 'Contact', 'Projects'];
 
 
 function Header() {
