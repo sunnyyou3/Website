43343e64883f465fea68806ddbb538a04e491c66
Sunny You <yousunny3@gmail.com>
2025-10-07 17:35:19 +0900

    added gradient

--- website/src/Components/Header.js
+++ website/src/Components/Header.js
@@ -11,31 +11,21 @@ import AboutContact from "../Pages/Contact";
 import Gallery from "../Pages/Gallery";
 import Projects from "../Pages/Projects";
 import { useState } from "react";
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
+import Title from '../Pages/Title';
+import { Container } from "@mui/system";
 
-const navItems = ['Home', 'Projects', 'Gallery', 'About', 'Contact'];
+const navItems = ['Home', 'Projects', 'Gallery', 'About', 'Contact', 'Game'];
 
 function Header() {
     const [page, setPage] = useState('');
-    const [drawerOpen, setDrawerOpen] = useState(false);
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-
-    const handleDrawerToggle = () => {
-        setDrawerOpen(!drawerOpen);
-    };
 
     const handleNavItemClick = (item) => {
         setPage(item);
-        if (isMobile) {
-            setDrawerOpen(false);
-        }
     };
 
     return (
         <Box id="header">
-            <AppBar id="AppBar" style={{ background: 'transparent', boxShadow: 'none' }}>
+            <AppBar id="AppBar" style={{ background: 'transparent', boxShadow: 'none', position: 'absolute' }}>
                 <Toolbar>
                     <Typography
                         variant="h6"
@@ -46,8 +36,8 @@ function Header() {
                     </Typography>
                    <Box sx={{ display: 'flex' }}>
                             {navItems.map((item) => (
-                                <Button class="header-buttons" variant="outlined" size="medium" onClick={() => handleNavItemClick(item)} key={item} 
-                                    sx={{ color: 'white', borderColor: 'white', margin: '10px 5px', borderRadius: '20px', padding: '1px 8px', height: '30px', 
+                                <Button variant="outlined" size="medium" onClick={() => handleNavItemClick(item)} key={item} 
+                                    sx={{ color: 'white', borderColor: 'white', margin: '10px 5px', borderRadius: '30px', padding: '1px 8px', height: '30px', 
                                         '&:hover': {
                                             color: 'black',
                                             backgroundColor: 'white'
@@ -68,6 +58,7 @@ function Header() {
                 else if (page === 'Gallery') return <Gallery />
                 else if (page === 'About') return <About />
                 else if (page === 'Contact') return <AboutContact />
+                else if (page === 'Game') return <Container> <Title /> </Container>
                 else return <Home />
             })()}
         </Box>
--- website/src/Pages/Main.js
+++ website/src/Pages/Main.js
@@ -1,18 +1,17 @@
-import React, { useEffect } from "react";
-import { Container } from "@mui/system";
 import Header from '../Components/Header';
 import MainPageImage from '../Components/MainPageImage';
-import { Button } from "@mui/material";
-import Title from "./Title";
+
+// Website inspiration from https://www.landing.love/sites/mikkisindhunata/
 
 function Main() {
     return(
-        <div id="main">
+        <div id="main" style={{backgroundColor: 'black', overflow: 'hidden', position: 'relative'}}>
             <div id="main-background-image" style={{zIndex: 1, position: 'absolute', height: '100vh', width: '100vw', overflow: 'hidden'}}>
                 <MainPageImage/>
             </div>
             <div id="main-content" style={{zIndex: 2, position: 'relative', backgroundColor: 'black', height: '100vh', width: '100vw', opacity: 0.5}} />
-            <Header />
+                <Header />
+            <div id="main-content-center" style={{zIndex: 2, position: 'relative', height: '100vh', width: '100vw', opacity: 0.8, bottom: '150px', marginBottom: '-150px'}} />
         </div>
     )
 }
