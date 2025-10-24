a155a06afd35829fc3555d804b3353ce2cae066d
Sunny You <yousunny3@gmail.com>
2025-10-24 18:17:34 +0900

    Added lightning effect to lightbulb

--- website/src/Components/Lightroom.js
+++ website/src/Components/Lightroom.js
@@ -1,6 +1,7 @@
 import React from "react";
 import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
 import { IconButton } from '@mui/material';
+import { motion } from "motion/react"
 
 const IconButtonStyle = {
     color: 'white', 
@@ -8,16 +9,19 @@ const IconButtonStyle = {
     minHeight: '100px',
     transform: 'rotate(180deg)',
     position: 'relative',
-    top: '150px',
-    marginRight: '150px'
+    top: '300px'
 }
+
+const lightningBefore = 'radial-gradient(circle,rgba(207, 207, 62, 1) 0%, rgba(77, 77, 61, 1) 8%, rgba(0, 0, 0, 1) 100%)';
+const lightningAfter = 'radial-gradient(circle,rgba(255, 255, 0, 1) 0%, rgba(168, 168, 99, 1) 33%, rgba(0, 0, 0, 1) 75%)';
 
 function Lightroom() {
     return(
-        <div align="right">
-            <IconButton sx={IconButtonStyle}
-                children = {<LightbulbOutlinedIcon sx={{width: '100px', height: '100px'}} />}
-            />
+        <div align="center">
+            <motion.div animate={{ background: [lightningBefore, lightningAfter, lightningBefore] }} transition={{duration: 5, repeat: Infinity}} style={{ position: 'relative', top:'520px', maxWidth: '300px', minHeight: '300px', borderRadius: '50%'}} />
+            <IconButton sx={{...IconButtonStyle }} component={motion.div} 
+                    children = {<LightbulbOutlinedIcon sx={{width: '100px', height: '100px'}} />}
+                />
         </div>        
     )
 }
