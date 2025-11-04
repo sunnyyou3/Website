174b7660f4f86d35e4cd3f05935464e6dfc65080
Sunny You <yousunny3@gmail.com>
2025-11-04 18:28:19 +0900

    Attempted at adding background room

--- website/src/Components/Lightroom.js
+++ website/src/Components/Lightroom.js
@@ -1,8 +1,8 @@
 import React from "react";
 import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
 import { IconButton } from '@mui/material';
-import { motion } from "motion/react"
-
+import { motion } from "motion/react";
+import roomImage from './../Images/Background/room.jpg'; // Import the image
 
 const IconButtonStyle = {
     color: 'white', 
@@ -11,20 +11,38 @@ const IconButtonStyle = {
     transform: 'rotate(180deg)',
     position: 'relative',
     top: '300px'
-}
+};
 
 const lightningBefore = 'radial-gradient(circle,rgba(207, 207, 62, 1) 0%, rgba(77, 77, 61, 1) 8%, rgba(0, 0, 0, 1) 100%)';
 const lightningAfter = 'radial-gradient(circle,rgba(255, 255, 0, 1) 0%, rgba(168, 168, 99, 1) 33%, rgba(0, 0, 0, 1) 75%)';
 
 function Lightroom() {
-    return(
+    const [showRoom, setShowRoom] = React.useState(false);
+
+    return (
         <div align="center">
-            <motion.div animate={{ background: [lightningBefore, lightningAfter, lightningBefore] }} transition={{duration: 5, repeat: Infinity}} style={{ position: 'relative', top:'520px', maxWidth: '300px', minHeight: '300px', borderRadius: '50%'}} />
-            <IconButton sx={{...IconButtonStyle }} component={motion.div} 
-                    children = {<LightbulbOutlinedIcon sx={{width: '100px', height: '100px'}} />}
+            {showRoom ?
+                <div 
+                    className="Lightroom-background" 
+                    style={{ position: 'relative', left:'100px', top: '150px', backgroundImage: `url(${roomImage})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', height: '100vh', width: '100vw' }} 
                 />
-        </div>        
-    )
+            : 
+                <div className="Lightbulb" style={{ position: 'relative', right: '80px' }}>
+                    <motion.div 
+                        animate={{ background: [lightningBefore, lightningAfter, lightningBefore] }} 
+                        transition={{ duration: 5, repeat: Infinity }} 
+                        style={{ position: 'relative', top: '520px', maxWidth: '300px', minHeight: '300px', borderRadius: '50%' }} 
+                    />
+                    <IconButton 
+                        sx={{ ...IconButtonStyle }} 
+                        component={motion.div} 
+                        children={<LightbulbOutlinedIcon sx={{ width: '100px', height: '100px' }} />}
+                        onClick={() => setShowRoom(true)}
+                    />
+                </div> 
+            }
+        </div>
+    );
 }
 
 export default Lightroom;
