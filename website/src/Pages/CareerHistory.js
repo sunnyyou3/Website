7689db25487e72e5308d7cf839d1fa16f5f9496f
Yuki Holmes <yuki@Yukis-MacBook-Pro.local>
2025-11-26 17:55:46 +0900

    Added text to career history page

--- website/src/Pages/CareerHistory.js
+++ website/src/Pages/CareerHistory.js
@@ -8,6 +8,7 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
+import testImage from './../assets/Portfolio/Images/1.jpg'
 
 const projectDate = [
 	"",
@@ -31,12 +32,12 @@ const projectTitle = [
 
 const projectText = [
 	"",
-	"Software Engineer Intern at FreeWheel", 
-	"hello from the other side hello from the other side hello from the other side hello from the other side hello from the other side", 
-	"hello from the other side hello from the other side hello from the other side hello from the other side hello from the other side", 
-	"hello from the other side hello from the other side hello from the other side hello from the other side hello from the other side", 
-	"hello from the other side hello from the other side hello from the other side hello from the other side hello from the other side",
-	""
+	"Inspired by my love of building computers, I decided to work for the LTS department to help troubleshoot computers. All computers first come through me, where I apply my experiences in diagnosing what the problem is and finding potential solutions. I would set up Jira tickets to track all repairs.",
+	"I was hired by the department of Computer Science to support young developers. I monitor and mentor 3 teams of 5 persons, teaching them the Agile methodology, Restful API, Google OAuth, dependency management, app deployment, frontend and backend connections, and designing and maintaining a database using a PaaS solution.",
+	"My first software developer position as an intern. Hired by Comcast and placed into FreeWheel to help the team upgrade their QA certification webpage. I learned a lot and made a lot of connections, so I am quite grateful for this opportunity.", 
+	"Promoted to Head Teaching Assistant. I now also take responsibility for other teaching assistants.", 
+	"Not only was I looking for an opportunity to develop applications using the skills and knowledge I've built but I also wanted to tackle the challenge of living in a new Country (Japan) and improving my Japanese language skills.",
+	"I was moved to a new team where my skills of being a developer and being fluent in English is better utilized. I obtained my JLPT N2 certification with the help of team members"
 ];
 
 function Image({ id, containerRef }) {
@@ -47,7 +48,7 @@ function Image({ id, containerRef }) {
 			<div ref={ref} style={{ borderRadius: '10px', overflow: 'hidden', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)'}}>
 				<motion.img
 					className='projectIMG'
-					src={{}}
+					src={{testImage}}
 					alt={`Project ${id}`}
 				/>
 			</div>
